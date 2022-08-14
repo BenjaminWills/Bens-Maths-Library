4bdfdc2878f49e9420feced9548a4373da3d2bd8
BenjaminWills <benjaminwills047@gmail.com>
2022-08-14 16:53:59 +0100

    Reformatted code + visualising transformed funcs

--- Mathematics fundamentals/visualisations.py
+++ Mathematics fundamentals/visualisations.py
@@ -1,13 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from linear_algebra import Linalg
 
 
 class Visualisation:
     def make_axis(self):
         fig, ax = plt.subplots()
-        return ax 
+        return ax
 
-    def matplotlib_config(self,ax,start,end):
+    def matplotlib_config(self, ax, start, end):
         ax.set_aspect("equal")
         ax.grid(True, which="both", linestyle=":")
         ax.spines["left"].set_position("zero")
@@ -16,21 +17,29 @@ class Visualisation:
         ax.spines["bottom"].set_position("zero")
         ax.spines["top"].set_color("none")
         ax.xaxis.tick_bottom()
-        ax.set_xlim(left = start - 1, right = end + 1, auto = True)
 
-    def get_function_visualisation(self, function, start, end, steps,ax):
+    def get_function_visualisation(self, function, start, end, steps, ax):
         if start >= end:
             return None
         plotting_space = np.linspace(start, end, steps)
         ax.plot(plotting_space, function(plotting_space))
-        self.matplotlib_config(ax,start,end)
+        self.matplotlib_config(ax, start, end)
 
-    def get_conic_visualisation(self,function,start,end,steps,ax):
+    def get_conic_visualisation(self, function, start, end, steps, ax):
         # Conic sections are one to many functions. So we need to plot them differently.
         plotting_space = np.linspace(start, end, steps)
         ax.plot(plotting_space, function(plotting_space)[0])
         ax.plot(plotting_space, function(plotting_space)[1])
-        self.matplotlib_config(ax,start,end)
+        self.matplotlib_config(ax, start, end)
+
+    def visualise_linear_transform(
+        self, function, start, end, steps, ax, transformation_matrix
+    ):
+        space = np.linspace(start, end, steps)
+        lin = Linalg()
+        points = lin.transform_function(space, function, transformation_matrix)
+        self.matplotlib_config(ax, start, end)
+        ax.scatter(points[0], points[1], marker=".", s=0.2)
 
     def show_visualisation(self):
         plt.show()
