25170fb245036c3d99aa8fab089737eb33b6eb6c
BenjaminWills <benjaminwills047@gmail.com>
2022-08-14 13:16:13 +0100

    Added multi plot functionality.

--- Mathematics fundamentals/visualisations.py
+++ Mathematics fundamentals/visualisations.py
@@ -1,19 +1,36 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 class Visualisation:
-    def get_function_visualisation(self, function, start, end, steps):
+    def make_axis(self):
+        fig, ax = plt.subplots()
+        return ax 
+
+    def matplotlib_config(self,ax,start,end):
+        ax.set_aspect("equal")
+        ax.grid(True, which="both", linestyle=":")
+        ax.spines["left"].set_position("zero")
+        ax.spines["right"].set_color("none")
+        ax.yaxis.tick_left()
+        ax.spines["bottom"].set_position("zero")
+        ax.spines["top"].set_color("none")
+        ax.xaxis.tick_bottom()
+        ax.set_xlim(left = start - 1, right = end + 1, auto = True)
+
+    def get_function_visualisation(self, function, start, end, steps,ax):
         if start >= end:
             return None
         plotting_space = np.linspace(start, end, steps)
-        fig, ax = plt.subplots()
         ax.plot(plotting_space, function(plotting_space))
-        ax.set_aspect('auto')
-        ax.grid(True, which='both',linestyle=':')
-        ax.spines['left'].set_position('zero')
-        ax.spines['right'].set_color('none')
-        ax.yaxis.tick_left()
-        ax.spines['bottom'].set_position('zero')
-        ax.spines['top'].set_color('none')
-        ax.xaxis.tick_bottom()
+        self.matplotlib_config(ax,start,end)
+
+    def get_conic_visualisation(self,function,start,end,steps,ax):
+        # Conic sections are one to many functions. So we need to plot them differently.
+        plotting_space = np.linspace(start, end, steps)
+        ax.plot(plotting_space, function(plotting_space)[0])
+        ax.plot(plotting_space, function(plotting_space)[1])
+        self.matplotlib_config(ax,start,end)
+
+    def show_visualisation(self):
         plt.show()
