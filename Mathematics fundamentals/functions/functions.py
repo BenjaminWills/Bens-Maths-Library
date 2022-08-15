7c249a6647767f9d288141b7d3eb1447b3aa1961
BenjaminWills <benjaminwills047@gmail.com>
2022-08-15 22:20:02 +0100

    Organising folders.

---
+++ Mathematics fundamentals/functions/functions.py
@@ -0,0 +1,81 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+class Functions:
+    def line(self, x, gradient, intercept):
+        return gradient * x + intercept
+
+    def polynomial(self, x, *coefficients):
+        output = 0
+        degree = len(coefficients) - 1
+        for index, coefficient in enumerate(coefficients):
+            output += coefficient * x ** (degree - index)
+        return output
+
+    def reciprocal(self, x, horizontal_shift=0, vertical_shift=0):
+        if x.any() == 0:
+            return 0
+        return 1 / (x - horizontal_shift) + vertical_shift
+
+    def step(self, x, horizontal_shift=0):
+        if x.any() == horizontal_shift:
+            return 1
+        else:
+            return 0
+
+    def sigmoid(self, x):
+        return 1 / (1 + np.exp(-x))
+
+    def sinh(self, x):
+        return 1 / 2 * (np.exp(x) - np.exp(-x))
+
+    def cosh(self, x):
+        return 1 / 2 * (np.exp(x) + np.exp(-x))
+
+    def tanh(self, x):
+        if x.any() == 0:
+            return 0
+        return self.sinh(x) / self.cosh(x)
+
+    def bell_curve(self, x, stretch=1, horizontal_shift=0):
+        return stretch * np.exp(-((x + horizontal_shift) ** 2))
+
+    def sin(self, x, frequency=1):
+        return np.sin(x / (2 * np.pi) * frequency)
+
+    def cos(self, x, frequency=1):
+        return np.cos(x / (2 * np.pi) * frequency)
+
+    def tan(self, x, frequency=1):
+        return np.tan(x / (2 * np.pi) * frequency)
+
+    def exp(self, x):
+        return np.exp(x)
+
+    def ln(self, x):
+        if x.any() <= 0:
+            return 0
+        return np.log(x)
+
+    def log_base_n(self, x, n):
+        return np.log(x) / np.log(n)
+
+    def circle(self, x, radius, centre_x, centre_y):
+        if (radius**2 - (x - centre_x) ** 2).any() <= 0:
+            return [x, 0]
+
+        pos_y_co_ordinate = centre_y + np.sqrt(radius**2 - (x - centre_x) ** 2)
+        neg_y_co_ordinate = centre_y - np.sqrt(radius**2 - (x - centre_x) ** 2)
+
+        return [neg_y_co_ordinate,pos_y_co_ordinate]
+
+    def hyperbola(self, x, major, minor):
+        y_co_ordinate = major * np.sqrt(1 + (x / minor) ** 2)
+        return [-y_co_ordinate, y_co_ordinate]
+
+    def elipse(self, x, major, minor):
+        if (1 - (x / minor) ** 2).any() <= 0:
+            return [x, 0]
+        y_co_ordinate = major * np.sqrt(1 - (x / minor) ** 2)
+        return [-y_co_ordinate, y_co_ordinate]
---
+++ Mathematics fundamentals/visualisations/visualisations.py
@@ -0,0 +1,58 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from linear_algebra import Linalg
+
+plt.style.use("dark_background")
+
+
+class Visualisation:
+    def make_axis(self):
+        fig, ax = plt.subplots()
+        return ax
+
+    def matplotlib_config(self, ax, set_y_lim=False, y_lim1=0, y_lim2=0):
+        ax.set_aspect("equal")
+        ax.grid(True, which="both", linestyle=":")
+        ax.spines["left"].set_position("zero")
+        ax.spines["right"].set_color("none")
+        ax.yaxis.tick_left()
+        ax.spines["bottom"].set_position("zero")
+        ax.spines["top"].set_color("none")
+        ax.xaxis.tick_bottom()
+        if set_y_lim:
+            ax.set_ylim(y_lim1, y_lim2)
+
+    def get_function_visualisation(self, function, start, end, steps, ax):
+        if start >= end:
+            return None
+        plotting_space = np.linspace(start, end, steps)
+        ax.plot(plotting_space, function(plotting_space))
+        self.matplotlib_config(ax)
+
+    def get_conic_visualisation(self, function, start, end, steps, ax):
+        # Conic sections are one to many functions. So we need to plot them differently.
+        plotting_space = np.linspace(start, end, steps)
+        ax.plot(plotting_space, function(plotting_space)[0])
+        ax.plot(plotting_space, function(plotting_space)[1])
+        self.matplotlib_config(ax)
+
+    def visualise_function_linear_transform(
+        self, function, start, end, steps, ax, transformation_matrix
+    ):
+        space = np.linspace(start, end, steps)
+        lin = Linalg()
+        points = lin.transform_function(space, function, transformation_matrix)
+        self.matplotlib_config(ax, set_y_lim=True, y_lim1=-2, y_lim2=10)
+        ax.scatter(points[0], points[1], marker=".", s=0.2)
+
+    def visualise_conic_linear_transform(self,function, start, end, steps, ax, transformation_matrix):
+        space = np.linspace(start, end, steps)
+        lin = Linalg()
+        pos_points = lin.transform_conic_function(space,function,transformation_matrix)[0]
+        neg_points = lin.transform_conic_function(space,function,transformation_matrix)[1]
+        self.matplotlib_config(ax)
+        ax.scatter(pos_points[0], pos_points[1], marker=".", s=0.2)
+        ax.scatter(neg_points[0], neg_points[1], marker=".", s=0.2)
+
+    def show_visualisation(self):
+        plt.show()
