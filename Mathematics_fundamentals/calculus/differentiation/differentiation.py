ec0bdce186ab8a3d15c80b9f7e076a7000554f59
BenjaminWills <benjaminwills047@gmail.com>
2022-08-20 21:49:06 +0100

    Added taylor series

--- Mathematics_fundamentals/calculus/differentiation/differentiation.py
+++ Mathematics_fundamentals/calculus/differentiation/differentiation.py
@@ -1,6 +1,8 @@
 import sys
 sys.path.append('/Users/benwills/Desktop/personal_projects/Mathematics_fundamentals')
 
+import numpy as np
+
 class Differentiation:
     def get_derivative(self,function,x):
         dx = 10 ** -5
@@ -8,17 +10,25 @@ class Differentiation:
         slope = vert_change/dx
         return slope
     
-    def newton_raphson_method(self,function,start,end,initial_point):
+    def newton_raphson_method(self,function,initial_point, verbose = False):
         x0 = initial_point
         max_iterations = 100000
         iteration_count = 0
         while abs(self.get_derivative(function,x0)) > 10 ** -5:
             if iteration_count == max_iterations:
-                return 'DIVERGENT'
+                return 'DIVERGENT: possibly a saddle point nearby.'
             slope = self.get_derivative(function,x0)
             func_value = function(x0)
             x0 = x0 - (func_value/slope)
             iteration_count += 1
-        return x0
+        x0 = np.round(x0,4)
+        if verbose:
+            return \
+                f"""Iteration count: {iteration_count}, root: ({x0},{function(x0)})"""
+
+        return [[x0],[function(x0)]]
+
+    def taylor_series(self,function,degree,centre):
+        pass
 
 
