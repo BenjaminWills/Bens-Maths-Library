1beebf3342a61bd8110dc5a7fc62798abcefb718
BenjaminWills <benjaminwills047@gmail.com>
2022-08-20 23:16:11 +0100

    added conjugate + magnitude functions

--- Mathematics_fundamentals/numbers/complex.py
+++ Mathematics_fundamentals/numbers/complex.py
@@ -1,5 +1,17 @@
+from real import Real
 
 class Complex:
     def __init__(self,real,imaginary):
         self.re = real
         self.im = imaginary
+
+    def get_conjugate(self):
+        return Complex(self.re,-self.im)
+        
+    def get_magnitude(self):
+        real = Real()
+        return real.sqrt(self.re ** 2 + self.im ** 2)
+
+z = Complex(1,4)
+z_bar = z.get_conjugate()
+print(z_bar.get_magnitude())
