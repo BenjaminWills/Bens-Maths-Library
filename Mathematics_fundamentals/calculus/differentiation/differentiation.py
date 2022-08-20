71a8083179ac689470e19aa8af56121eaad7e2dc
BenjaminWills <benjaminwills047@gmail.com>
2022-08-20 22:13:59 +0100

    added recursive nth derivative function.

--- Mathematics_fundamentals/calculus/differentiation/differentiation.py
+++ Mathematics_fundamentals/calculus/differentiation/differentiation.py
@@ -1,7 +1,9 @@
 import sys
 sys.path.append('/Users/benwills/Desktop/personal_projects/Mathematics_fundamentals')
 
+# from numbers.real import Real
 import numpy as np
+import math 
 
 class Differentiation:
     def get_derivative(self,function,x):
@@ -23,12 +25,29 @@ class Differentiation:
             iteration_count += 1
         x0 = np.round(x0,4)
         if verbose:
-            return \
-                f"""Iteration count: {iteration_count}, root: ({x0},{function(x0)})"""
-
+            return f"""Iteration count: {iteration_count}, root: ({x0},{function(x0)})"""
         return [[x0],[function(x0)]]
 
-    def taylor_series(self,function,degree,centre):
-        pass
+    def get_nth_derivative(self,function,x,order = 1):
+        # at each point we need to find another derivative of a derivative.
+        if order == 1:
+            return self.get_derivative(function,x)
+        else:
+            def deriv(x):
+                return self.get_derivative(function,x)
+            return self.get_nth_derivative(deriv,x,order-1)
+        
+
+    def taylor_series(self,function,x,order,centre = 0):
+        output = function(x)
+        for i in range(1,order):
+            output += self.get_nth_derivative(function,x,order) * (x - centre) ** order / math.factorial(order)
+        return output
 
 
+diff = Differentiation()
+
+def polynomial(x):
+    return x**3 
+
+print(diff.get_nth_derivative(polynomial,5,2))
