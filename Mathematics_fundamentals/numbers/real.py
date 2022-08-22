89b6bf60b5a37f11a2f51bfbd18bb497bce71e5a
BenjaminWills <benjaminwills047@gmail.com>
2022-08-22 22:21:19 +0100

    added power set function

--- Mathematics_fundamentals/numbers/real.py
+++ Mathematics_fundamentals/numbers/real.py
@@ -1,4 +1,4 @@
-from complex import Complex
+# from complex import Complex
 import numpy as np
 
 class Real:
@@ -13,3 +13,25 @@ class Real:
             return 1
         else:
             return x * self.factorial(x-1)
+
+    def get_powerset(self,arr:list):
+        """
+        This function will find the set of all subsets of an array (excluding the empty set),
+        for an array of length n the power set has a length of 2 ** n - 1.
+
+        for [1,2] the power set is [],[1],[2],[1,2]. Note that it has 4 elements.
+        """
+        power_set = []
+        n = len(arr)
+        for i in range(2 ** n):
+            sub_set = []
+            for j in range(n):
+                if i & 2 ** j:
+                    sub_set.append(arr[j])
+            power_set.append(sub_set)
+        return power_set
+
+num = Real()
+list = [1,2,3]
+power_set = num.get_powerset(list)
+print(power_set)
