6b91c961fa71036000be748a8c966db8bf69cb9f
BenjaminWills <benjaminwills047@gmail.com>
2022-08-27 09:45:03 +0100

    added multiplication function to complex

--- Mathematics_fundamentals/numbers/complex.py
+++ Mathematics_fundamentals/numbers/complex.py
@@ -1,4 +1,5 @@
 from real import Real
+import numpy as np
 
 class Complex:
     def __init__(self,real,imaginary):
@@ -8,10 +9,23 @@ class Complex:
     def get_conjugate(self):
         return Complex(self.re,-self.im)
         
+    def get_complex_number(self):
+        return f'{self.re}{"+" if self.im >= 0 else ""}{self.im}i'
+
     def get_magnitude(self):
         real = Real()
         return real.sqrt(self.re ** 2 + self.im ** 2)
 
-z = Complex(1,4)
-z_bar = z.get_conjugate()
-print(z_bar.get_magnitude())
+    def get_argument(self):
+        return np.arctan(self.im/self.re)
+
+    def multiply_complex(self,num):
+        if isinstance(num,Complex):
+                return Complex(self.re * num.re - self.im * num.im, self.re * num.im + self.im * num.re)
+        else:
+            return Complex(num * self.re,num * self.im)
+    
+a = Complex(4,2)
+b = a.get_conjugate()
+# print(a.get_complex_number(),b.get_complex_number())
+print(a.multiply_complex(b).get_complex_number())
