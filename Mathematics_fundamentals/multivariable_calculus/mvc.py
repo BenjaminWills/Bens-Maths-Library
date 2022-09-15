eafe3cf288027a084ab0a00998fca8fb45ba37f3
BenjaminWills <benjaminwills047@gmail.com>
2022-09-15 22:52:55 +0100

    Added laplacian and fixed divergence

--- Mathematics_fundamentals/multivariable_calculus/mvc.py
+++ Mathematics_fundamentals/multivariable_calculus/mvc.py
@@ -1,3 +1,6 @@
+from ast import Call
+from gc import callbacks
+from os import stat
 import sys
 from typing import Callable
 
@@ -6,21 +9,23 @@ sys.path.append('/Users/benwills/Desktop/personal_projects/Mathematics_fundament
 from linear_algebra import Vector
 
 class MVC:
+
     @staticmethod
     def get_partial_derivative(x:Vector,function:Callable,component:int) -> float:
         """
         Will get the derivative of a scalar valued function f(x,y,z,...) w.r.t the specified
         component
         """
-        dx = 10 ** -8
+        dx = 10 ** -5
         unit_vector = Vector.get_unit_vector(position = component,dimension = x.dim)
         function_diff = function(x+unit_vector*dx) - function(x)
         gradient = function_diff/dx
         return gradient
+
     @staticmethod
     def get_gradient(x:Vector,function:Callable) -> Vector:
         """
-        Will return a gradient vector of a multivariate function. (the vector of partial derivatives)
+        Will return a gradient vector of a scalar multivariate function. (the vector of partial derivatives)
         """
         dim = x.dim
         gradient_list = []
@@ -29,14 +34,55 @@ class MVC:
             gradient_list.append(partial_derivative)
         return Vector(*gradient_list)
 
+    @staticmethod
+    def get_laplacian(x:Vector,function:Callable) -> float:
+        """
+        Will return a laplacian of a scalar multivariate function. (the sum of second partial derivatives)
+        """
+        def gradient(y:Vector):
+            return MVC.get_gradient(y,function)
+        return Vector_Calculus.get_divergence(x,gradient)
 
+            
+    # @staticmethod
+    # def pure_gradient_descent(x:Vector,function:Callable,)
+
+class Vector_Calculus:
+    
+    @staticmethod
+    def find_ith_derivative(x:Vector,position:int,function:Callable) -> float:
+        """
+        Will find the derivative of the i'th component of a vector valued function
+        i.e many co-ordinates to many co-ordinates.
+        """
+        dx = 10 ** -5
+        grad = function(x+Vector.get_unit_vector(position,x.dim)*dx) - function(x)
+        component = Vector.unpack_vector(grad)[position]
+        return component/dx
+
+    @staticmethod
+    def get_divergence(x:Vector,function:Callable) -> float:
+        """
+        Will get the divergence of a vector field.
+        """
+        gradients = [
+            Vector_Calculus.find_ith_derivative(x,position,function)
+            for position in range(x.dim)
+            ]
+        return sum(gradients)
 
 
 if __name__ == '__main__':
-    def f(v):
-        x,y,z = Vector.unpack_vector(v)
-        return x + 2*y - z
+    def f(x): return x ** 2
+    def g(x): return x/2
 
-    v = Vector(1,1,1)
-    deriv = MVC.get_gradient(v,f)
-    print(deriv.vector)
+    def F(vector):
+        x,y = Vector.unpack_vector(vector)
+        return Vector(f(x),g(y))
+
+    x = Vector(1,1,1,1)
+    def f(vector):
+        x,y,z,w = Vector.unpack_vector(vector)
+        return x ** 2 + y ** 2 + z ** 2 + w ** 2
+
+    print(MVC.get_laplacian(x,f))
