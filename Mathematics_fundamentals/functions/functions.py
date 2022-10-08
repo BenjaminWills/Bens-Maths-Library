a86fcae0f4320beae652ce6322ab2b0931bdb3d6
BenjaminWills <benjaminwills047@gmail.com>
2022-10-08 17:19:09 +0100

    refactor

--- Mathematics_fundamentals/functions/functions.py
+++ Mathematics_fundamentals/functions/functions.py
@@ -1,84 +1,98 @@
-
 import numpy as np
 
-from Mathematics_fundamentals.numbers.complex import Complex
+from Mathematics_fundamentals.numbers.numbers import Complex
 
 
 class Functions:
-    def line(self, x, gradient, intercept):
+    @staticmethod
+    def line(x, gradient, intercept):
         return gradient * x + intercept
 
-    def polynomial(self, x, *coefficients):
+    @staticmethod
+    def polynomial(x, *coefficients):
         output = 0
         degree = len(coefficients) - 1
         for index, coefficient in enumerate(coefficients):
             output += coefficient * x ** (degree - index)
         return output
 
-    def reciprocal(self, x, horizontal_shift=0, vertical_shift=0):
+    @staticmethod
+    def reciprocal(x, horizontal_shift=0, vertical_shift=0):
         if x.any() == 0:
             return 0
         return 1 / (x - horizontal_shift) + vertical_shift
 
-    def step(self, x, horizontal_shift=0):
+    @staticmethod
+    def step(x, horizontal_shift=0):
         if x.any() == horizontal_shift:
             return 1
         else:
             return 0
 
-    def sigmoid(self, x):
+    @staticmethod
+    def sigmoid(x):
         return 1 / (1 + np.exp(-x))
 
-    def sinh(self, x):
-        return (self.exp(x) - self.exp(x * -1)) * 0.5
+    @staticmethod
+    def sinh(x):
+        return (Functions.exp(x) - Functions.exp(x * -1)) * 0.5
 
-    def cosh(self, x):
-        return (self.exp(x) + self.exp(x * -1)) * 0.5
+    @staticmethod
+    def cosh(x):
+        return (Functions.exp(x) + Functions.exp(x * -1)) * 0.5
 
-    def tanh(self, x):
+    @staticmethod
+    def tanh(x):
         if x.any() == 0:
             return 0
-        return self.sinh(x) / self.cosh(x)
+        return Functions.sinh(x) / Functions.cosh(x)
 
-    def bell_curve(self, x, stretch=1, horizontal_shift=0):
+    @staticmethod
+    def bell_curve(x, stretch=1, horizontal_shift=0):
         return stretch * np.exp(-((x + horizontal_shift) ** 2))
 
-    def sin(self, x, frequency=1):
+    @staticmethod
+    def sin(x, frequency=1):
         return np.sin(x / (2 * np.pi) * frequency)
 
-    def cos(self, x, frequency=1):
+    @staticmethod
+    def cos(x, frequency=1):
         return np.cos(x / (2 * np.pi) * frequency)
 
-    def tan(self, x, frequency=1):
+    @staticmethod
+    def tan(x, frequency=1):
         return np.tan(x / (2 * np.pi) * frequency)
 
-    def exp(self, x):
+    @staticmethod
+    def exp(x):
         if isinstance(x, Complex):
             re = x.re
             im = x.im
             return Complex(np.exp(re) * np.cos(im), np.exp(re) * np.sin(im))
         return np.exp(x)
 
-    def ln(self, x):
+    @staticmethod
+    def ln(x):
         if x <= 0:
             return 0
         return np.log(x)
 
-    def log_base_n(self, x, n):
+    @staticmethod
+    def log_base_n(x, n):
         return np.log(x) / np.log(n)
 
-    def circle(self, x, radius, centre_x, centre_y):
+    @staticmethod
+    def circle(x, radius, centre_x, centre_y):
         pos_y_co_ordinate = centre_y + np.sqrt(radius**2 - (x - centre_x) ** 2)
         neg_y_co_ordinate = centre_y - np.sqrt(radius**2 - (x - centre_x) ** 2)
-
         return [neg_y_co_ordinate, pos_y_co_ordinate]
 
-    def hyperbola(self, x, major, minor):
+    @staticmethod
+    def hyperbola(x, major, minor):
         y_co_ordinate = major * np.sqrt(1 + (x / minor) ** 2)
         return [-y_co_ordinate, y_co_ordinate]
 
-    def elipse(self, x, major, minor):
-        # if (1 - (x / minor) ** 2).any() <= 0:
-        #     return [x, 0]
+    @staticmethod
+    def elipse(x, major, minor):
         y_co_ordinate = major * np.sqrt(1 - (x / minor) ** 2)
         return [-y_co_ordinate, y_co_ordinate]
--- Mathematics_fundamentals/numbers/numbers.py
+++ Mathematics_fundamentals/numbers/numbers.py
@@ -1,6 +1,44 @@
 import numpy as np
 
-from Mathematics_fundamentals.numbers.complex import Complex
+
+class Complex:
+    def __init__(self, real, imaginary):
+        self.re = real
+        self.im = imaginary
+
+    def __add__(self, other):
+        if isinstance(other, Complex):
+            return Complex(self.re + other.re, self.im + other.im)
+        else:
+            return complex(other + self.re, self.im)
+
+    def __sub__(self, other):
+        if isinstance(other, Complex):
+            return Complex(self.re - other.re, self.im - other.im)
+        else:
+            return complex(self.re - other, self.im)
+
+    def __mul__(self, other):
+        if isinstance(other, Complex):
+            return Complex(
+                self.re * other.re - self.im * other.im,
+                self.re * other.im + self.im * other.re,
+            )
+        else:
+            return Complex(other * self.re, other * self.im)
+
+    def get_conjugate(self):
+        return Complex(self.re, -self.im)
+
+    def get_complex_number(self):
+        return f'{self.re}{"+" if self.im >= 0 else ""}{self.im}i'
+
+    def get_magnitude(self):
+        real = Real()
+        return real.sqrt(self.re**2 + self.im**2)
+
+    def get_argument(self):
+        return np.arctan(self.im / self.re)
 
 
 class Real:
@@ -10,11 +48,12 @@ class Real:
         else:
             return np.sqrt(x)
 
-    def factorial(self, x):
+    @staticmethod
+    def factorial(x):
         if x == 0:
             return 1
         else:
-            return x * self.factorial(x - 1)
+            return x * Real.factorial(x - 1)
 
     def get_powerset(self, arr: list):
         """
