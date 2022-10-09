73c42a171175d82c34e125c28c2553ee22335f36
BenjaminWills <benjaminwills047@gmail.com>
2022-10-09 22:40:51 +0100

    Fixed code

--- applications/2d_linear_regression.py
+++ applications/2d_linear_regression.py
@@ -3,25 +3,24 @@ Linear regression beginning with 2-d
 """
 TRAIN_NUM = 700
 TEST_NUM = 200
-import csv
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
 
 from Mathematics_fundamentals.linear_algebra.linear_algebra import Vector
 from Mathematics_fundamentals.multivariable_calculus.mvc import MVC
 
-with open("applications/test.csv", newline="") as f:
-    reader = csv.reader(f)
-    training_data = list(reader)
+training_data = pd.read_csv('/Users/benwills/Desktop/personal_projects/applications/train.csv')
+testing_data = pd.read_csv('/Users/benwills/Desktop/personal_projects/applications/test.csv')
 
-with open("applications/train.csv", newline="") as f:
-    reader = csv.reader(f)
-    test_data = list(reader)
+training_data_dict = training_data.to_dict()
+training_data_list = [[training_data_dict['x'][i],training_data_dict['y'][i]] for i in range(TRAIN_NUM)]
 
-def cost_function(vector: Vector, data: list = training_data[:TRAIN_NUM]) -> float:
+def cost_function(vector: Vector, data: list = training_data_list[:TRAIN_NUM]) -> float:
     m, c = Vector.unpack_vector(vector)
     n = len(data)
     sum = 0
     for i in range(n):
-
         sum += (m * float(data[i][0]) + c - float(data[i][1])) ** 2
     return sum / n
 
@@ -32,7 +31,7 @@ optimal_values = MVC.hybrid_backtracking_newton_gradient(
     verbose=True,
     max_iterations=2000,
     beta = 0.3,
-    alpha = 0.01
+    alpha = 0.1
 )
 
 m, c = Vector.unpack_vector(optimal_values)
@@ -41,14 +40,8 @@ print(m,c)
 def line_of_best_fit(x):
     return m * x + c
 
-
-import matplotlib.pyplot as plt
-import numpy as np
-
-x  = [test_data[i][0] for i in range(TEST_NUM)]
-y = [test_data[i][1] for i in range(TEST_NUM)]
-
 space = np.linspace(0, 100, 10000)
-plt.scatter(x,y)
-plt.plot(space, line_of_best_fit(space))
+
+plt.scatter(testing_data.iloc[:, 0], testing_data.iloc[:, -1])
+plt.plot(space,line_of_best_fit(space), color = 'red')
 plt.show()
